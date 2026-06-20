e9731a032fec1ef2a1e289ab6903d4a2d283bb78
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-20 14:27:07 -0400

    .

--- challenges/constructors/src/starter.rs
+++ challenges/constructors/src/starter.rs
@@ -2,8 +2,20 @@ pub struct Book {
     // 1. Define the fields of the struct
     // Make all of them public with `pub`
     // Read the description for the fields
+    pub title: String,
+    pub author: String,
+    pub year: i32,
+    pub likes: i32,
 }
 
 impl Book {
     // 2. Define the `new` associated function
+    pub fn new(title: String, author: String, year: i32, likes: i32) -> Self {
+        Self {
+            title,
+            author,
+            year,
+            likes,
+        }
+    }
 }
