7320f4137b81d15c7ba0a10a11a5d69f9eb16810
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-20 13:53:15 -0400

    .

--- challenges/functions/src/starter.rs
+++ challenges/functions/src/starter.rs
@@ -1,5 +1,6 @@
 pub fn add(a: i32, b: i32) -> i32 {
     // Step 1: implement addition
+    a+b
 }
 
 // Step 2:
@@ -7,9 +8,14 @@ pub fn add(a: i32, b: i32) -> i32 {
 // that accepts two arguments of type `i32`
 // and returns an `i32`.
 // make sure you make the function public by using the `pub` keyword.
-
+pub fn subtract(a: i32, b: i32) -> i32 {
+    a-b
+}
 // Step 3:
 // Define a public function named `multiply`
 // that accepts two arguments of type `i32`
 // and returns an `i32`.
 // make sure you make the function public by using the `pub` keyword.
+pub fn multiply(a: i32, b: i32) -> i32 {
+    a*b
+}
