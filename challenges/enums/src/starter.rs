37fc8b7a18930b393c30402ec2865257652633d8
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-20 15:03:07 -0400

    .

--- challenges/enums/src/starter.rs
+++ challenges/enums/src/starter.rs
@@ -1,7 +1,15 @@
 pub enum TrafficLight {
     // Define enum variants here
+    Red,
+    Yellow,
+    Green,
 }
 
 pub fn light_action(light: &TrafficLight) -> &'static str {
     // Your code here...
+    match light {
+        TrafficLight::Red => "Stop",
+        TrafficLight::Yellow => "Caution",
+        TrafficLight::Green => "Go",
+    }
 }
--- challenges/playing-cards/src/starter.rs
+++ challenges/playing-cards/src/starter.rs
@@ -1,7 +1,17 @@
 pub enum Card {
     // Define the Card variants here
+    King,
+    Queen,
+    Jack,
+    Numbered(u8, String)
 }
 
 pub fn card_description(card: &Card) -> String {
     // Your code here...
+    match card {
+        Card::King => String::from("King"),
+        Card::Queen => String::from("Queen"),
+        Card::Jack => String::from("Jack"),
+        Card::Numbered(x, y) => format!("{} of {}", x, y)
+    }
 }
