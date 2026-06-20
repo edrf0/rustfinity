909e3bf4dff3f201a13fa5b3e7b675d67ef6548c
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-20 14:55:08 -0400

    .

--- challenges/unit-structs/src/starter.rs
+++ challenges/unit-structs/src/starter.rs
@@ -1,6 +1,13 @@
 // Define a struct named `Logger`
 // Implement an associated function `log_message`
 // That accepts a `&str` and prints the output.
+struct Logger;
+
+impl Logger {
+    fn log_message(s: &str) {
+        println!("{}", s);
+    }
+}
 
 // Example usage:
 pub fn main() {
