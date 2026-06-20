7c406f6d5e28bd7af41430db6e2e2d77a8f2c284
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-20 15:09:47 -0400

    .

--- challenges/methods-on-enums/src/starter.rs
+++ challenges/methods-on-enums/src/starter.rs
@@ -1,14 +1,27 @@
 pub enum VehicleStatus {
     // Define the VehicleStatus variants here
+    Parked,
+    Driving { speed: u32 },
+    BrokenDown(String),
 }
 
 impl VehicleStatus {
     pub fn is_operational(&self) -> bool {
         // Your code here...
+        match self {
+            VehicleStatus::Parked => true,
+            VehicleStatus::Driving { .. } => true,
+            VehicleStatus::BrokenDown(_) => false,
+        }
     }
 
     pub fn description(&self) -> String {
         // Your code here...
+        match self {
+            VehicleStatus::Parked => "The vehicle is parked.".to_string(),
+            VehicleStatus::Driving { speed } => format!("The vehicle is driving at {} km/h.", speed),
+            VehicleStatus::BrokenDown(reason) => format!("The vehicle is broken down: {}.", reason),
+        }
     }
 }
 
