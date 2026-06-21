3335464858a4a9bb96807ee5fe51353738bbeba1
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-20 22:27:03 -0400

    .

--- challenges/animal-sanctuary-registry/src/starter.rs
+++ challenges/animal-sanctuary-registry/src/starter.rs
@@ -4,12 +4,15 @@ type Collection = HashMap<String, Vec<String>>;
 
 pub fn add_animal_to_section(animal: &str, section: &str, registry: &mut Collection) {
     // TODO: implement this function
+    registry.entry(String::from(section)).or_insert(Vec::new()).push(String::from(animal));
 }
 
 pub fn get_animals_in_section(section: &str, registry: &Collection) -> Vec<String> {
     // TODO: implement this function
+    registry.get(&String::from(section)).unwrap_or(Vec::new())
 }
 
 pub fn get_all_animals_sorted(registry: &Collection) -> Vec<String> {
     // TODO: implement this function
+    registry.keys().sorted().collect::<Vec<String>>()
 }
