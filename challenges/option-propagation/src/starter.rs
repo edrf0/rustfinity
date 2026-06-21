f753913366aec516ef46d03aee873b0fb2e0d33c
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-21 13:55:14 -0400

    .

--- challenges/option-propagation/src/starter.rs
+++ challenges/option-propagation/src/starter.rs
@@ -2,8 +2,8 @@ pub fn find_and_multiply(numbers: Vec<i32>, index1: usize, index2: usize) -> Opt
     // TODO: Instead of using `unwrap`, use the `?` operator to propagate the option
     // HINT: `numbers.get` returns a Option<i32> value
 
-    let num1 = numbers.get(index1).unwrap();
-    let num2 = numbers.get(index2).unwrap();
+    let num1 = numbers.get(index1)?;
+    let num2 = numbers.get(index2)?;
     Some(num1 * num2)
 }
 
