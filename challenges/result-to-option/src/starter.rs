a708ff70061b0a996dedde5bebaf9513ae5986e4
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-21 14:00:46 -0400

    .

--- challenges/result-to-option/src/starter.rs
+++ challenges/result-to-option/src/starter.rs
@@ -1,6 +1,11 @@
 pub fn read_file(file_path: &str) -> Option<String> {
     // TODO: Implement this function
     // Hint: Use `File::open` and `.read_to_string()` with `?` to propagate errors.
+    let file_handle = File::open(file_path)?;
+    let reader = BufReader::new(file_handle);
+    let mut file_contents = String::new();
+    reader.read_to_string(&mut file_contents)?;
+    Some(file_contents)
 }
 
 // Example usage
