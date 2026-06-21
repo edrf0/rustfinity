37bd286ebafe30bd00a805ed94be895662e83f81
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-21 15:35:25 -0400

    .

--- challenges/associated-types/src/starter.rs
+++ challenges/associated-types/src/starter.rs
@@ -1,12 +1,41 @@
+/*Define a trait KeyValueStore with:
+
+An associated type Key.
+An associated type Value.
+Methods:
+set to add a key-value pair.
+get takes a reference of &Key and returns Option<&Value>.
+Create a struct InMemoryStore that uses a HashMap to store key-value pairs.
+Implement the KeyValueStore trait for this struct.
+
+Make sure all relevant values are public so that they can be accessed from
+outside the module (essential to pass the tests).*/
+
 // 1. Finish the trait definition
-pub trait KeyValueStore
+pub trait KeyValueStore {
+    type Key;
+    type Value;
+    fn get(&self, key: &Self::Key) -> Option<&Self::Value>;
+    fn set(&mut self, key: Self::Key, value: Self::Value);
+}
 
 // 2. Implement the trait for InMemoryStore
 // Make sure the fields are public
-pub struct InMemoryStore
+pub struct InMemoryStore {
+    pub memory: HashMap<String, Box<dyn KeyValueStore>>,
+}
 
 // 3. Implement the trait for InMemoryStore
-
+impl KeyValueStore for InMemoryStore {
+    type Key = String;
+    type Value = String;
+    fn get(&self, key: &Self::Key) -> Option<&Self::Value> {
+        self.memory.get(key)
+    }
+    fn set(&mut self, key: Self::Key, value: Self::Value) {
+        self.memory.insert(key.to_owned(), Box::new(value));
+    }
+}
 // Example usage
 pub fn main() {
     let mut store: InMemoryStore<String, String> = InMemoryStore {
