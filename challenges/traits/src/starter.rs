4a83c8ecec332a5e995614160a33bb1343fe999b
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-21 14:27:47 -0400

    .

--- challenges/traits/src/starter.rs
+++ challenges/traits/src/starter.rs
@@ -13,6 +13,17 @@ pub struct Book {
 }
 
 // TODO: Implement the `Describable` trait for `Person` and `Book`
+impl Describable for Person {
+    fn describe(&self) -> String {
+        format!("Person: {}, Age: {}", self.name, self.age)
+    }
+}
+
+impl Describable for Book {
+    fn describe(&self) -> String {
+        format!("Book: {}, Author: {}", self.title, self.author)
+    }
+}
 
 // Example usage
 pub fn main() {
