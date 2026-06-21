6019576238e1d99d21653b14194cf7f88a1482af
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-21 17:40:00 -0400

    .

--- challenges/the-drop-trait/src/starter.rs
+++ challenges/the-drop-trait/src/starter.rs
@@ -1,3 +1,7 @@
+/*Create TempFile struct with a method new that takes a file name as input, it must accept both &str
+and String types.
+Implement the Drop trait for TempFile to delete the file automatically when the struct goes out of scope.*/
+
 use std::path::PathBuf;
 
 pub struct TempFile {
@@ -6,12 +10,15 @@ pub struct TempFile {
 
 impl TempFile {
     // 1. Define the `new` associated function
+    pub fn new(path: PathBuf) -> Self {
+        Self { path }
+    }
 }
 
 impl Drop for TempFile {
     fn drop(&mut self) {
         // Your code here to delete the file when TempFile is dropped
-        unimplemented!()
+        fs::remove_file(&self.path)
     }
 }
 
