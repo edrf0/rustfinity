09a890214357d9de1c851ca2ef31d3b64b566d62
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-21 17:33:16 -0400

    .

--- challenges/box-t/src/starter.rs
+++ challenges/box-t/src/starter.rs
@@ -1,3 +1,8 @@
+/*Implement the create_animal function to return a Box<Animal> containing a new Animal instance.
+
+Define another function, access_animal, that takes a Box<Animal> and returns a tuple (String, u8)
+representing the animal's name and age. Use dereferencing to access the fields.*/
+
 pub struct Animal {
     pub name: String,
     pub age: u8,
@@ -5,10 +10,15 @@ pub struct Animal {
 
 pub fn create_animal(name: &str, age: u8) -> Box<Animal> {
     // Your code here
+    Box::new(Animal {
+        name: name.to_string(),
+        age
+    })
 }
 
 pub fn access_animal(animal: Box<Animal>) -> (String, u8) {
     // Your code here
+    (*animal.name, *animal.age)
 }
 
 // Example usage
