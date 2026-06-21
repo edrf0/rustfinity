7a7ba94cb53b2f02c2362621085a675177d21032
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-21 15:18:30 -0400

    .

--- challenges/return-trait-objects/src/starter.rs
+++ challenges/return-trait-objects/src/starter.rs
@@ -1,10 +1,45 @@
+/*Define the Speakable trait with a method speak that returns a String.
+Define a struct Dog with two fields: name and breed, both of type String.
+Implement the Speakable trait for Dog to return a string Woof.
+Define a struct Robot with two fields: model and purpose, both of type String.
+Implement the Speakable trait for Robot to return a string Beep boop.
+Finish the function get_speaker that takes a &str parameter and returns either a Dog
+or a Robot based on the parameter.
+The parameter can either be dog or robot.*/
+trait Speakable {
+    fn speak(&self) -> String;
+}
+
+struct Dog {
+    name: String,
+    breed: String
+}
+
+impl Speakable for Dog {
+    fn speak(&self) -> String {
+        String::from("Woof")
+    }
+}
+
+struct Robot {
+    model: String,
+    purpose: String,
+}
+
+impl Speakable for Robot {
+    fn speak(&self) -> String {
+        String::from("Beep boop")
+    }
+}
 pub fn get_speaker(kind: &str) -> Box<dyn Speakable> {
     match kind {
         "dog" => {
             // Return a Dog instance here
+            Box::new(Dog {})
         }
         "robot" => {
             // Return a Robot instance here
+            Box::new(Robot {})
         }
         _ => panic!("Unknown speaker type"),
     }
