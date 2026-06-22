e608e6fdca4f84ec1fe4456b508511d2019bd7db
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-21 23:43:39 -0400

    .

--- challenges/temperature-converter/src/starter.rs
+++ challenges/temperature-converter/src/starter.rs
@@ -1,3 +1,9 @@
 pub fn convert_temperature(value: f64, from_unit: &str, to_unit: &str) -> Result<f64, String> {
-    // TODO: Implement the function here
+    match (from_unit, to_unit) {
+        ("C","F") => value * (9.0 / 5.0) + 32.0,
+        ("F","C") => (value - 32.0) * (5.0 / 9.0),
+        ("C","K") => value + 273.15,
+        ("K","C") => value - 273.15,
+        _ => Err(format!("Invalid unit")),
+    }
 }
