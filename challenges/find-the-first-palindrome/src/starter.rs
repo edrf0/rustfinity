e76620cae5ceefafa2286f22b8d00e2528ec67aa
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-22 00:21:37 -0400

    .

--- challenges/find-the-first-palindrome/src/starter.rs
+++ challenges/find-the-first-palindrome/src/starter.rs
@@ -1,5 +1,26 @@
 pub fn find_first_palindrome(start: i32, end: i32) -> Option<i32> {
-    // TODO: Implement the function here
-
+    if start > end {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    if end < 0 {
+        return None;
+    }
+    let is_palindrome;
+    for number in start..=end {
+        is_palindrome = true;
+        let number_str: String = number.to_string();
+        for index in 0..(number_str.len() / 2) {
+            if number_str.chars().nth(index).unwrap() !=
+                number_str.chars().nth(number_str.len() - 1 - index).unwrap() {
+                is_palindrome = false;
+                break;
+            }
+        }
+        if is_palindrome {
+            return Some(number);
+        }
+    }
     None
 }
