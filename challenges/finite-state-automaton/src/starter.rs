b0256d411efa3c779717d073fa1b5337ca3b50ac
Emir Ravelo <125705907+edrf0@users.noreply.github.com>
2026-06-22 00:29:42 -0400

    .

--- challenges/finite-state-automaton/src/starter.rs
+++ challenges/finite-state-automaton/src/starter.rs
@@ -1,5 +1,17 @@
 pub fn recognize_pattern(input: &str) -> bool {
-    // Implement your finite state automaton here
-
-    false
+    if input.len() < 2 {
+        return false;
+    }
+    if input.chars().nth(0).unwrap() != 'a' || input.chars().nth(input.len()-1).unwrap() != 'c' {
+        return false;
+    }
+    if input == "ac" {
+        return true;
+    }
+    for index in 1..(input.len() - 1) {
+        if input.chars().nth(index).unwrap() != 'b' {
+            return false;
+        }
+    }
+    true
 }
